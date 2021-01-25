dbb272934faf1b301962f5fde9b3511993031bab
Abraham <AO613782@achmea.nl>
2021-01-25 17:05:47 +0100

    form added

--- my-api/src/app/app.component.ts
+++ my-api/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-api';
+  loadedPosts = [];
+
+  constructor() {};
+
+  onSubmit(value: any) {
+    console.log(value);
+  }
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; coments: string}) {
+    console.log(postData);
+  }
+
+  
 }
